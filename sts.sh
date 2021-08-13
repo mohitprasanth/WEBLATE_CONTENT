bd7238b2482cfeb611ae7fc36a8e0847ed29ea53
MOHIT PRASANTH KALAHASTHI <22170424+mohitprasanth@users.noreply.github.com>
2021-08-13 09:44:52 +0530

    Update sts.sh

--- sts.sh
+++ sts.sh
@@ -4,7 +4,7 @@ unset AWS_SESSION_TOKEN
 
 token=$1
 
-CREDS=$(aws sts get-session-token --serial-number arn:aws:iam::485239875118:mfa/mohitprasanth@joveo.com --token-code $token --duration 129600)
+CREDS=$(aws sts get-session-token --serial-number arn:aws:iam::812144555506:mfa/mohitprasanth@joveo.com --token-code $token --duration 129600)
 
 ACCES_KEY=$(echo $CREDS | python -c 'import sys, json; print json.load(sys.stdin)["Credentials"]["AccessKeyId"]')
 SECRET_ACCESS_KEY=$(echo $CREDS | python -c 'import sys, json; print json.load(sys.stdin)["Credentials"]["SecretAccessKey"]')
